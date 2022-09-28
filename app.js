12644a1ecb05f1d997c46197597267fa5588f09e
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-09-28 12:49:08 -0500

    ✅ successfully connected express to html files

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/public/views");
+app.set("view engine", "html");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// parse application/json
+app.use(bodyParser.json());
+// parse the raw data
+app.use(bodyParser.raw());
+// parse text
+app.use(bodyParser.text());
+
+app.use(bodyParser.json());
 
 mongoose.connect(
   "mongodb+srv://freeswapadmin:cafemacisgreat@freeswap.nx7crsb.mongodb.net/?retryWrites=true&w=majority",
@@ -18,23 +39,27 @@ const DB = mongoose.connection.useDb("freeswap");
 
 // Creating bug schema and Model 📝
 
-const bugSchema = mongoose.Schema({
-  name: String,
-  description: String,
-});
+// const bugSchema = mongoose.Schema({
+//   name: String,
+//   description: String,
+// });
 
-const Bug = DB.model("bug", bugSchema, "items");
+// const Bug = DB.model("bug", bugSchema, "items");
 
-const firstBug = new Bug({
-  name: "items ",
-  description:
-    "creating a mongoose data base using the url but cannot find it in the mongo show db in the terminal",
-});
+// const firstBug = new Bug({
+//   name: "items ",
+//   description:
+//     "creating a mongoose data base using the url but cannot find it in the mongo show db in the terminal",
+// });
 
-firstBug.save();
+// firstBug.save();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home2.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("home");
 });
 
 app.listen(port, () => {
